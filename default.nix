9bd43a6157a6fae22d3303a40a4d25683926192e
Neil Locketz <neil@nlocketz.com>
2020-08-23 13:19:56 -0400

    Nix build

---
+++ default.nix
@@ -0,0 +1,14 @@
+let
+  pkgs = import <nixpkgs> {};
+in (pkgs.rustPlatform.buildRustPackage rec {
+  pname = "lockjaw";
+  version = "0.1.0";
+  src = ./.;
+  buildInputs = [ pkgs.keyutils ];
+  cargoSha256 = "sha256:1gj46681w6h3qksmjzryg17gds9lvvy71x77xz7i2gkzzqksjb5b";
+  meta = with pkgs.stdenv.lib; {
+    description = "CLI Secret Manager with Linux Keyring Support";
+    platforms = platforms.linux;
+    license = licenses.bsd3;
+  };
+})
